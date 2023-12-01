beb4609445daa8ac4a4c8504694ddae4f7162139
IsraelChidera <israelchidera54@gmail.com>
2023-12-01 22:35:17 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#3600ff',
+        secondary: '#161616'
+      }
     },
   },
   plugins: [],
